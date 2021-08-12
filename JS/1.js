a7ce58f7ded8605f485b17c5580b5a6daf8936de
VeraBohatyrenko <bogatyrenkovera@gmail.com>
2021-08-13 00:36:55 +0300

    автопроверка

--- JS/1.js
+++ JS/1.js
@@ -389,3 +389,45 @@
 //генерация чисел от min до max
 
 //
+
+//Объявление переменных
+//<ключевое слово> <имя переменной> = <значение></значение>
+// const age = 20;
+// const username = "Mango";
+// const socialProfileTag = "@gluck";
+// const totalWorkerSalary = 4052;
+
+// //Переопределение переменных
+// let age = 5;
+// age = 10;
+// let username = "Mango";
+// username = "Poly";
+
+// // Числа
+// const age = 20;
+// const salary = 3710.84;
+
+// // Строки
+// const name = "Mango";
+// const description = "JavaSript essentials";
+
+// // Були
+// const isModalOpen = true;
+// const isLoggedIn = false;
+// //Математические операторы
+// const x = 10;
+// const y = 5;
+
+// // Сложение
+// console.log(x + y); // 15
+// // Вычитание
+// console.log(x - y); // 5
+// // Умножение
+// console.log(x * y); // 50
+// // Деление
+// console.log(x / y); // 2
+
+// //Шаблонные строки
+// const guestName = "Mango";
+// const roomNumber = 207;
+// const greeting = `Добро пожаловать ${guestName}, вы поселены в номер ${roomNumber}`;
